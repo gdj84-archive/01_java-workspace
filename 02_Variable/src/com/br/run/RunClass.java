dc28ca0c12bf2d146cc4861f16b0f7b115dbfc1d
gdj84-archive <lecturekbr@naver.com>
2024-06-18 15:50:08 +0900

    update

--- 02_Variable/src/com/br/run/RunClass.java
+++ 02_Variable/src/com/br/run/RunClass.java
@@ -7,7 +7,8 @@ public class RunClass {
 	public static void main(String[] args) {
 		// Variable 클래스 내의 whyUseVariable 메소드 호출
 		Variable variable = new Variable();
-		variable.whyUseVariable();
+		//variable.whyUseVariable();
+		variable.declareVariable();
 	}
 
 }
--- 02_Variable/src/com/br/variable/Variable.java
+++ 02_Variable/src/com/br/variable/Variable.java
@@ -70,7 +70,7 @@ public class Variable {
 		 */
 		
 		/*
-		 * < 기본 자료형 (primitive type) >
+		 * < 기본 자료형 (primitive type) - 8개 >
 		 * 1. 논리형 : boolean(1byte)
 		 * 2. 정수형 : byte(1byte), short(2byte), int(4byte), long(8byte)
 		 * 3. 실수형 : float(4byte), double(8byte)
@@ -78,16 +78,77 @@ public class Variable {
 		 * 
 		 * < 참조 자료형 (reference type) >
 		 * 1. 문자열 : String
-		 * 
+		 * ... 기본자료형을 제외한 모든 자료형들 ..
 		 */
 		
+		
+		boolean bool1;			// 변수 선언
+		bool1 = true;			// 변수에 값 대입
+		boolean bool2 = false;	// 변수 선언과 동시에 초기화
+		
+		System.out.println("bool1의 값 : " + bool1);
+		System.out.println("bool2의 값 : " + bool2);
+		
+		byte b = 100;   	// -128 ~ 127
+		short s = 32000;	// -32xxx ~ 32xxx
+		int i = 2100000000; // -21억xxxx ~ 21억xxxx		=> 정수형중에 대표자료형(기본형)
+		long l = 10000000000000L; // 엄청큼
+		
+		System.out.println("b의 값 : " + b);
+		System.out.println("s의 값 : " + s);
+		System.out.println("i의 값 : " + i);
+		System.out.println("l의 값 : " + l);
+		
+		float f = 4.12345678901234567f; // 4byte
+		double d = 8.12345678901234546; // 8byte => 실수형중에 가장 대표적인 자료형 (더 정확한 값을 담을 수 있기 때문에)
+		
+		System.out.println("f의 값 : " + f);
+		System.out.println("d의 값 : " + d);
+		
+		char c1 = 'a'; // 0 ~ 65xxx 범위 
+		char c2 = '힇';
+		
+		System.out.println("c1의 값 : " + c1);
+		System.out.println("c2의 값 : " + c2);
+		System.out.println("c1의 숫자값 : " + (int)c1);
+		System.out.println("c2의 숫자값 : " + (int)c2);
+		
+		System.out.println((int)'강');
+		
+		String str = "안녕하세요"; // String => 참조자료형 
+		System.out.println("str의 값 : " + str);
+		
+	}
+	
+	public void namingRule() {
+		/*
+		 * < 변수명명규칙 >
+		 * 
+		 * 1. 동일한 영역({})에는 중복된 변수 선언 불가
+		 * 2. 예약어(자바에서 이미 사용하고 있는 키워드들) 사용 불가
+		 * 3. 숫자 사용가능 (단, 숫자로 시작은 불가)
+		 * 4. 특수문자는 _, $만 가능
+		 * 5. Camel Case로 작성 권장 
+		 * 6. 한글 권장 x
+		 */
+		
+		int age;
+		//double age;
+		int aGe;
+		
+		//int true;
+		//int public;
+		//int class;
+		//int void;
+		
+		int num1;
+		int nu1m;
+		//int 1num;
 		
 		
 		
 		
 	}
-	
-	
 	
 	
 	
