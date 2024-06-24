23b6cda3d9d5094e139f7f4e9c797ab6c411474d
gdj84-archive <lecturekbr@naver.com>
2024-06-24 10:14:47 +0900

    조건문 실습문제

---
+++ 04_Control/src/com/br/chap01/practice/run/Run.java
@@ -0,0 +1,11 @@
+package com.br.chap01.practice.run;
+
+import com.br.chap01.practice.example.ConditionPracticeAnswer;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new ConditionPracticeAnswer().practice9();
+	}
+
+}
