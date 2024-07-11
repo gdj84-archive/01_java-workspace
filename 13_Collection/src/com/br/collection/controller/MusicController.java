fef12a9de34b6218e32e82ac23cbe742d1b97461
gdj84-archive <lecturekbr@naver.com>
2024-07-11 16:47:03 +0900

    update

--- 13_Collection/src/com/br/collection/controller/MusicController.java
+++ 13_Collection/src/com/br/collection/controller/MusicController.java
@@ -13,6 +13,7 @@ public class MusicController {
 		list.add(new Music("Good Bye", "박효신"));
 		list.add(new Music("술이 달다", "에픽하이"));
 		list.add(new Music("밥이 달다", "강보람"));
+		list.add(new Music("우산", "에픽하이"));
 	}
 	
 	// 각 요청별 실행될 메소드
@@ -47,6 +48,71 @@ public class MusicController {
 		
 	}
 	
+	public int updateMusic(String title, String upTitle, String upArtist) {
+		
+		int result = 0;
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				list.get(i).setTitle(upTitle);
+				list.get(i).setArtist(upArtist);
+				result++;
+				break;
+			}
+		}
+		
+		// result = 0(수정할 곡을 찾지 못함) | 1(성공적으로 수정됨)
+		return result;
+		
+	}
+	
+	public List<Music> selectMusicByKeyword(String keyword) {
+		
+		// 검색된 Music객체를 담기 위한 리스트 생성 
+		List<Music> searchList = new ArrayList<>(); // 텅 빈 상태
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().contains(keyword)) {
+				//System.out.println(list.get(i));
+				searchList.add(list.get(i));
+			}
+		}
+		
+		// searchList = 텅빈상태(검색결과가 없었음) | 텅비어있지 않음(검색결과가 있음)
+		return searchList;
+		
+	}
+	
+	
+	// 가수명을 통해서 검색된 곡명들 반환 
+	public List<String> searchTitleByArtist(String artist) {
+		
+		List<String> searchTitle = new ArrayList<>(); // 텅빈상태
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getArtist().equals(artist)) {
+				//System.out.println(list.get(i).getTitle());
+				searchTitle.add(list.get(i).getTitle());
+			}
+		}
+		
+		return searchTitle;
+		
+	}
+	
+	/*
+	 * 곡명을 전달받아 
+	 * 해당 곡명이 포함되어있는게 총 몇개인지 반환 
+	 */
+	public int selectCountByTitle(String title) {
+		
+		int count = 0;
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().replace(" ", "").contains(title.replace(" ", ""))) {
+				count++;
+			}
+		}
+		
+		return count;
+		
+	}
 	
 	
 	
--- 13_Collection/src/com/br/collection/view/MusicView.java
+++ 13_Collection/src/com/br/collection/view/MusicView.java
@@ -25,6 +25,8 @@ public class MusicView {
 			System.out.println("3. 특정 곡 삭제");
 			System.out.println("4. 특정 곡 검색");
 			System.out.println("5. 특정 곡 수정");
+			System.out.println("6. 가수명을 통해서 곡명 찾기");
+			System.out.println("7. 특정 곡 갯수 조회");
 			System.out.println("0. 프로그램 종료");
 			
 			System.out.print(">> 메뉴 선택 : ");
@@ -35,8 +37,10 @@ public class MusicView {
 			case 1: inputMusic(); break;
 			case 2: selectMusic(); break;
 			case 3: removeMusic(); break;
-			case 4: /**/ break;
-			case 5: /**/ break;
+			case 4: searchMusic(); break;
+			case 5: modifyMusic(); break;
+			case 6: searchMusicTitle(); break;
+			case 7: searchCount(); break;
 			case 0: System.out.println("프로그램을 종료합니다. 이용해주셔서 감사합니다."); return;
 			default: System.out.println("메뉴를 잘못 선택하셨습니다. 다시 선택해주세요.");
 			}
@@ -90,13 +94,92 @@ public class MusicView {
 		int result = mc.deleteMusic(title); // 0 | 1
 		
 		if(result > 0) {
-			
+			System.out.println("성공적으로 삭제되었습니다.");
 		}else {
-			
+			System.out.println("삭제할 곡을 찾지 못했습니다.");
 		}
 		
 	}
 	
+	// 특정곡을 찾아 수정하는 서브화면
+	public void modifyMusic() {
+		
+		System.out.println("\n=== 특정 곡 수정 화면 ===");
+		
+		System.out.print("수정하고자하는 곡명 : ");
+		String title = sc.nextLine();
+		
+		System.out.print("수정 내용(곡명) : ");
+		String upTitle = sc.nextLine();
+		
+		System.out.print("수정 내용(가수명) : ");
+		String upArtist = sc.nextLine();
+		
+		// 수정 요청 보내기! (Controller측 메소드 호출)
+		int result = mc.updateMusic(title, upTitle, upArtist); // 0 | 1
+		
+		if(result > 0) {
+			System.out.println("성공적으로 수정되었습니다.");
+		}else {
+			System.out.println("수정할 곡을 찾지 못했습니다.");
+		}
+		
+	}
 	
+	public void searchMusic() {
+		
+		System.out.println("\n=== 특정 곡 검색 화면 ===");
+		
+		System.out.print("검색할 곡명(키워드) : ");
+		String keyword = sc.nextLine();
+		
+		List<Music> searchList = mc.selectMusicByKeyword(keyword);
+		
+		
+		if(searchList.isEmpty()) {
+			System.out.println("검색된 결과가 없습니다.");
+		}else {
+			for(int i=0; i<searchList.size(); i++) {
+				System.out.println(searchList.get(i));
+			}
+		}
+		
+	}
+	
+	public void searchMusicTitle() {
+		
+		System.out.println("\n=== 가수명을 통해 곡명 찾기 ===");
+		
+		/*
+		 * 사용자에게 가수명을 입력받아 
+		 * 현재 list내의 Music객체들 중 해당 가수명과 일치하는 
+		 * 곡명들을 출력하시오.
+		 */
+		System.out.print("검색할 가수명 : ");
+		String artist = sc.nextLine();
+		
+		List<String> searchTitle = mc.searchTitleByArtist(artist);
+		
+		if(searchTitle.isEmpty()) { // 비어있을 경우 == 검색결과없음
+			System.out.println(artist + "에 대한 검색 결과가 없습니다.");
+		}else { // 그게아닐경우 == 검색결과있음
+			for(String title : searchTitle) {
+				System.out.println(title);
+			}
+		}
+		
+	}
+	
+	public void searchCount() {
+		System.out.println("\n=== 특정 곡이 몇개인지 ===");
+		
+		System.out.print("곡명 : ");
+		//String title = sc.nextLine();
+		//int count = mc.selectCountByTitle(title);
+		int count = mc.selectCountByTitle(sc.nextLine());
+		
+		System.out.println("조회된 곡이 " + count + "개 입니다.");
+		
+	}
 
 }
